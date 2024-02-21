8b56c214df4637087d7805f1bf20171061ac3241
LiewZhengXian <zhengxianliew107@gmail.com>
2024-02-21 19:40:59 +0800

    Initialize Project

---
+++ OOPP_Assignment/src/Product.java
@@ -0,0 +1,13 @@
+
+public class Product {
+	private String productName;
+	private double productPrice;
+	private int productQuantity;
+	private int productItemNumber;
+	private boolean productStatus = true;
+	
+	
+	
+
+}
+

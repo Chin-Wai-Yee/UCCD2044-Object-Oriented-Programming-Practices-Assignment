d9d3617c603100c26ddb5e9c865f4efc9ba428a1
lol <zhengxianliew107@gmail.com>
2024-02-22 12:38:21 +0800

    Added Construtors,getters to Product.java

--- OOPP_Assignment/src/Product.java
+++ OOPP_Assignment/src/Product.java
@@ -6,7 +6,33 @@ public class Product {
 	private int productItemNumber;
 	private boolean productStatus = true;
 	
-	
+	//Constructor
+	public Product(String productName, double productPrice, int productQuantity, int productItemNumber, boolean productStatus){
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productQuantity = productQuantity;
+		this.productItemNumber = productItemNumber;
+		this.productStatus = productStatus;
+	}
+
+	//Getters
+	public String getProductName(){
+		return productName;
+	}
+	public double getProductPrice(){
+		return productPrice;
+	}
+	public int getProductQuantity(){
+		return productQuantity;
+	}
+	public int getProductItemNumber(){
+		return productItemNumber;
+	}
+	public boolean getProductStatus(){
+		return productStatus;
+	}
+
+	//Setters 
 	
 
 }

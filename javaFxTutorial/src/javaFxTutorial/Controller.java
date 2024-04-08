7ecedbb5ccaeb3c0484a69b25ad06655df865c77
LZX <zhengxianliew107@gmail.com>
2024-04-08 11:18:35 +0800

    uh

---
+++ javaFxTutorial/src/javaFxTutorial/Controller.java
@@ -0,0 +1,16 @@
+package javaFxTutorial;
+import javafx.event.ActionEvent;
+public class Controller {
+	public void up(ActionEvent e) {
+		System.out.println("UP");
+	}
+	public void left(ActionEvent e) {
+		System.out.println("LEFT");
+	}
+	public void right(ActionEvent e) {
+		System.out.println("RIGHT");
+	}
+	public void down(ActionEvent e) {
+		System.out.println("DOWN");
+	}
+}
--- javaFxTutorial/src/javaFxTutorial/Main.java
+++ javaFxTutorial/src/javaFxTutorial/Main.java
@@ -1,18 +1,22 @@
 package javaFxTutorial;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
-	public void start(Stage  primaryStage) throws Exception{
-		Parent root = new BorderPane();
+	public void start(Stage  primaryStage) {
+		try{
+		Parent root  =  FXMLLoader.load(getClass().getResource("/Main.fxml")); 
 		primaryStage.setTitle("Tutorial");
-		primaryStage.setScene(new Scene(root,300,275,Color.BLACK));
+		primaryStage.setScene(new Scene(root));
+	
 		primaryStage.show();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	public static void main (String[] args) {
 		launch(args);

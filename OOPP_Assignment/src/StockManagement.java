387f20a698741f267172edabbcb8f2c77ac775a6
LZX <zhengxianliew107@gmail.com>
2024-04-25 13:56:52 +0800

    s

--- OOPP_Assignment/src/StockManagement.java
+++ OOPP_Assignment/src/StockManagement.java
@@ -10,28 +10,9 @@ public class StockManagement {
     private static Scanner scanner = new Scanner(System.in);
     private static int productTotalNumber = 0; //increase by 1 when add 1 products
     private static int productMaxNumber;
-<<<<<<< HEAD
-    
-    private static final String ALPHA_NUM_REGEX = "[a-zA-Z0-9]+";
-    private static final String NUMBER_REGEX  ="[0-9]+";
-    private static final String DECIMAL_REGEX = "[0-9]*\\\\.?[0-9]*";
-    private static String getInput(String msg, String regex, String err) {
-    	String input;
-    	boolean flag;
-    	do {
-    		System.out.print(msg);
-    		input = scanner.nextLine().trim();
-    		flag =  input.matches(regex);
-    		if(!flag) {
-    			System.out.println("Error: "+ err);
-    		}
-    	}while(!flag);
-    	return input;
-    }
 
-=======
     private static String name;
->>>>>>> ef8b15694fb6615de23d2e8f3b55bf6dce1d50a5
+
     public static int getMaxProducts(Scanner scanner) {
     	System.out.println("======================================================");
     	System.out.println("||                                                  ||");
